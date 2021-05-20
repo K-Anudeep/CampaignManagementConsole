05b717454a730b890391ba2a74fb5677ce79ee60
saiteja kanchi <saiteja.kanchi@capgemini.com>
2021-05-20 16:07:35 +0530

    adding campaign entity

---
+++ Marketing Campaign Management/Entities/Campaign.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class Campaign
+    {
+        public int CampaignID { get; set; }
+        public string Name { get; set; }
+        public string Venue { get; set; }
+        public int AssignedTo { get; set; }
+        public DateTime StartedOn { get; set; }
+        public DateTime CompletedOn { get; set; }
+        public bool IsOpen { get; set; } 
+    }
+}
