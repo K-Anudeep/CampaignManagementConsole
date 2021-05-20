4cc8a6cfa8abf26d508a1564ee485599b76ef30a
saiteja kanchi <saiteja.kanchi@capgemini.com>
2021-05-20 16:14:46 +0530

    changing the class access

--- Marketing Campaign Management/Entities/Campaign.cs
+++ Marketing Campaign Management/Entities/Campaign.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entities
 {
-    class Campaign
+    public class Campaign
     {
         public int CampaignID { get; set; }
         public string Name { get; set; }
