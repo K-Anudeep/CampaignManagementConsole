050290c1dc9788ecb7f57b768ac7e25c068d0cd7
Harshavardhan Reddy <harsha83999@gmail.com>
2021-05-21 13:24:24 +0530

    Changes made to  leads class

--- Marketing Campaign Management/Entities/Leads.cs
+++ Marketing Campaign Management/Entities/Leads.cs
@@ -14,33 +14,76 @@ namespace Entities
             get { return _LeadID; }
             set { _LeadID = value; }
         }
+
+
         private int _CampaignID;
         public int CampaignID
         {
             get { return _CampaignID; }
             set { _CampaignID = value; }
         }
-        private string _ConsumerName;
 
-        public string ConsumerName { get => _ConsumerName; set => _ConsumerName = value; }
-        private string emailAddress;
-        public string EmailAddress { get => emailAddress; set => emailAddress = value; }
+
+        private string _ConsumerName;
+        public string ConsumerName
+        {
+            get { return _ConsumerName; }
+            set { _ConsumerName = value; }
+        }
+
+
+
+        private string _EmailAddress;
+        public string EmailAddress
+        {
+            get { return _EmailAddress; }
+            set { _EmailAddress = value; }
+        }
+
+
 
         private int _PhoneNo;
-        public int PhoneNo { get => _PhoneNo; set => _PhoneNo = value; }
+        public int PhoneNo
+        {
+            get { return _PhoneNo; }
+            set { _PhoneNo = value; }
+        }
+
+
 
         private string _PreferredMoC;
-        public string PreferredMoC { get => _PreferredMoC; set => _PreferredMoC = value; }
+        public string PreferredMoC
+        {
+            get { return _PreferredMoC; }
+            set { _PreferredMoC = value; }
+        }
+
+
 
         private DateTime _DateApproached;
-        public DateTime DateApproached { get => _DateApproached; set => _DateApproached = value; }
+        public DateTime DateApproached
+        {
+            get { return _DateApproached; }
+            set { _DateApproached = value; }
+        }
+
+
 
         private int _ProductID;
-        public int ProductID { get => _ProductID; set => _ProductID = value; }
+        public int ProductID
+        {
+            get { return _ProductID; }
+            set { _ProductID = value; }
+        }
+
+
 
         private string _Status;
-        public string Status { get => _Status; set => _Status = value; }
-
+        public string Status
+        {
+            get { return _Status; }
+            set { _Status = value; }
+        }
       
  
      
