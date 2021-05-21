51ac2efd47839e9ae4db54fc59f7a3fe0812f01f
SaiAnudeepK <anudeeph8@gmail.com>
2021-05-22 00:04:16 +0530

    Adding Business Layer files

---
+++ Marketing Campaign Management/BusinessLayer/Services/ExecMenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using DatabaseLayer;
+
+namespace BusinessLayer.Services
+{
+    public class ExecMenu
+    {
+    }
+}
