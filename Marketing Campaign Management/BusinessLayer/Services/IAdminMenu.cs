5d22f85c6df38c23da8ef34b82af60493b0ec97d
SaiAnudeepK <anudeeph8@gmail.com>
2021-05-22 09:36:29 +0530

    Starting Business Layer

--- Marketing Campaign Management/BusinessLayer/Services/IAdminMenu.cs
+++ Marketing Campaign Management/BusinessLayer/Services/IAdminMenu.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities;
+using DatabaseLayer;
 
 namespace BusinessLayer
 {
     public interface IAdminMenu
     {
+        bool AddProducts(Products products);
+
+        List<Products> DisplayProducts();
+
+        Products OneProduct(int pId);
+
+        bool DeleteProduct(int pId);
+
+        bool AddCampaign(Campaigns campaign);
+
+        List<Campaigns> ViewCampaigns();
+
+        bool CloseCampaign(int cId);
+
+        bool AddUsers(Users user);
+
+        List<Users> DisplayUsers();
+
+        Users OneUser(int uId);
+
+        bool DiscontinueUser(int uId);
+
+        List<Leads> ViewLeads();
+
+        Leads GetALead(int LeadID);
+
+
     }
 }
---
+++ Marketing Campaign Management/BusinessLayer/Validations/LoginCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using DatabaseLayer;
+
+namespace BusinessLayer.Validations
+{
+    public class LoginCheck
+    {
+        public bool Validation()
+        { 
+            try
+            {
+                throw new NotImplementedException();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+    }
+}
