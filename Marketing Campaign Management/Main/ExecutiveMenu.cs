3505a7f04d85306139003fa99fd9e61a8a2b93e5
SaiAnudeepK <anudeeph8@gmail.com>
2021-05-24 12:23:14 +0530

    Encryption of Connection done

--- Marketing Campaign Management/Main/ExecutiveMenu.cs
+++ Marketing Campaign Management/Main/ExecutiveMenu.cs
@@ -223,7 +223,6 @@ namespace PresentationLayer
                         {
                             Console.WriteLine("--------------------------------------------------------------------------");
                             throw new Exception("Error Campaign Not Assigned to you or Campaign is Closed!");
-                            Console.WriteLine("--------------------------------------------------------------------------");
                         }
                         break;
                     case 2:
